03e28a12fb8db4df9fb6b80b4234916da385aaaa
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2026-01-22 09:19:07 -0600

    wip

---
+++ src/Controller/SchedulerDebugController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+
+class SchedulerDebugController extends AbstractController
+{
+    public function __construct(
+        private CacheInterface $cache,
+    ) {
+    }
+
+    #[Route('/scheduler/last-run', name: 'scheduler_last_run', methods: ['GET'])]
+    public function lastRun(): JsonResponse
+    {
+        $payload = $this->cache->get('scheduler_heartbeat_last_run', fn () => null);
+
+        return $this->json([
+            'last_run' => $payload,
+        ]);
+    }
+}
